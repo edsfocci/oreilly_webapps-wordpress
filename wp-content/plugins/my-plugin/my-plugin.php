5e25e88a525d4cb2451b3c0e50bfd84622883dd0
Ed Solis <edsfocci@gmail.com>
2015-04-06 21:14:55 -0500

    Working with WordPress hooks

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -5,4 +5,13 @@
 * Author: Ed Solis
 * Author URI: https://github.com/edsfocci
 */
+?>
+
+<?php
+function my_plugin_wp_footer() {
+  echo 'I read Building Web Apps with WordPress
+    and now I am a WordPress Genius!';
+}
+add_action( 'wp_footer', 'my_plugin_wp_footer' );
+?>
 
